542e9df02f96a5acd7830a6051d760dd678a86b1
WarNote <andre_20113meira@hotmail.com>
2020-09-22 06:49:20 -0300

    Changed the filter method

--- tests/index.ts
+++ tests/index.ts
@@ -5,7 +5,7 @@ const blackListFiles = ['index.ts', 'generalInputs.ts'];
 
 const testFunctions = fsPromises.readdir('./tests')
   .then((files: string[]) => files
-    .filter((fileName) => !blackListFiles.includes(fileName))
+    .filter((fileName) => fileName[0] !== '_' && !blackListFiles.includes(fileName))
     .map((fileName) => import(`./${fileName}`)))
   .then((tests) => Promise.all(tests))
   .then((tests) => tests.map((test) => test.default));
