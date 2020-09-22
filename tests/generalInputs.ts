0fe1233a541b78dfef5f62db0f9a04dbc5204d1f
WarNote <andre_20113meira@hotmail.com>
2020-09-22 06:49:07 -0300

    Added and separated the functions

--- tests/generalInputs.ts
+++ tests/generalInputs.ts
@@ -1,7 +1,14 @@
 const inputs: string[] = [
   'ignis', 'valkyr', 'rhino', 'valkyr prime', 'aksomati', 'soma',
   'soma prime', 'akboltor', 'flow', 'carrier', 'carrier prime',
-  // wrong names input
+];
+
+const wrongInputs: string[] = [
   'valkkity', 'aksometi', 'carriar', 'flaw', 'suma', 'iginis',
 ];
-export const getRandomInput = (): string => inputs[Math.round(Math.random() * (inputs.length - 1))];
+
+const getRandomItem = (list: string[]) => list[Math.round(Math.random() * (inputs.length - 1))];
+
+export const getRandomInput = (): string => getRandomItem(inputs.concat(wrongInputs));
+export const getRandomRightInput = (): string => getRandomItem(inputs);
+export const getRandomWrongInput = (): string => getRandomItem(wrongInputs);
