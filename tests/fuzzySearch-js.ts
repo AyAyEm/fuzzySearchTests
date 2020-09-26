96d62f388ffb597348bea34c390f4a608d693953
WarNote <andre_20113meira@hotmail.com>
2020-09-26 08:57:17 -0300

    Addition of fuzzysearch-js to the tests

---
+++ tests/fuzzySearch-js.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import Test from './_baseTest';
+
+const FuzzySearchJs = require('fuzzysearch-js');
+// const levenshteinFS = require('fuzzysearch-js/js/modules/LevenshteinFS');
+const indexOfFS = require('fuzzysearch-js/js/modules/IndexOfFS');
+const wordCountFS = require('fuzzysearch-js/js/modules/WordCountFS');
+
+class FuzzySearchJsTest extends Test {
+  public testName = 'Fuzzysearch-js';
+
+  private fuzzySearchJs = new FuzzySearchJs(
+    this.items.map(({ namePrepared }) => namePrepared) as string[],
+    { minimumScore: 1 },
+  );
+
+  private nameIndexMap = new Map(this.items.map(
+    ({ namePrepared }, index) => [namePrepared, index],
+  ));
+
+  constructor(public times: number, public inputsList: string[]) { super(); }
+
+  preparationFunc() {
+    // this.fuzzySearchJs.addModule(levenshteinFS());
+    this.fuzzySearchJs.addModule(indexOfFS());
+    this.fuzzySearchJs.addModule(wordCountFS());
+  }
+
+  async testFunc(input: string) {
+    return this.fuzzySearchJs.search(input).map(
+      ({ value }: { value: string }) => this.items[this.nameIndexMap.get(value) as number],
+    );
+  }
+}
+
+export default function fuzzySearchJsTest(times: number, inputList: string[]) {
+  return new FuzzySearchJsTest(times, inputList).exec();
+}
