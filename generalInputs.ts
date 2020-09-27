88ed68f01abeb2c0fccca7361d1b6a41460bdbb7
WarNote <andre_20113meira@hotmail.com>
2020-09-27 11:03:42 -0300

    Removed unecessary code

--- generalInputs.ts
+++ generalInputs.ts
@@ -1,14 +1,5 @@
+// eslint-disable-next-line import/prefer-default-export
 export const inputs: string[] = [
   'ignis', 'valkyr', 'rhino', 'valkyr prime', 'aksomati', 'soma',
   'soma prime', 'akboltor', 'flow', 'carrier', 'carrier prime',
 ];
-
-export const wrongInputs: string[] = [
-  'valkkity', 'aksometi', 'carriar', 'flaw', 'suma', 'iginis',
-];
-
-const getRandomItem = (list: string[]) => list[Math.round(Math.random() * (inputs.length - 1))];
-
-export const getRandomInput = (): string => getRandomItem(inputs.concat(wrongInputs));
-export const getRandomRightInput = (): string => getRandomItem(inputs);
-export const getRandomWrongInput = (): string => getRandomItem(wrongInputs);
