4b4e0809e7ceac0c44d1f3e877b92a16b90fd6c2
WarNote <andre_20113meira@hotmail.com>
2020-09-28 09:58:06 -0300

    Added one input to make it even

--- src/utils/inputs.ts
+++ src/utils/inputs.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const inputs: string[] = [
   'ignis', 'valkyr', 'rhino', 'valkyr prime', 'aksomati', 'soma',
-  'soma prime', 'akboltor', 'flow', 'carrier', 'carrier prime',
+  'soma prime', 'akbolto', 'flow', 'carrier', 'carrier prime',
+  'akbolto prime',
 ];
