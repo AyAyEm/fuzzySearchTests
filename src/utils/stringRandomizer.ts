f44739ce63e0eb0e30aba727cf119f8496635c8e
WarNote <andre_20113meira@hotmail.com>
2020-09-28 16:15:12 -0300

    Updated stringRandomizer to fix edge cases

--- src/utils/stringRandomizer.ts
+++ src/utils/stringRandomizer.ts
@@ -1,14 +1,31 @@
-import _ from 'lodash';
+function randomNumber(lower = 0, upper = 1): number {
+  return Math.round((Math.random() * (upper - lower)) + lower);
+}
 
 /**
  * Rolls n times random letters in the string
  */
-
 export function stringRandomizer(input: string, times = 1): string {
-  const result: string[] = input.split('');
-  for (let i = 0; i < times; i += 1) {
-    result[_.random(0, result.length - 1)] = String.fromCharCode(_.random(97, 122));
+  if (!/[a-z]/gi.test(input)) {
+    throw new Error(`Invalid input string: ${input} must have at least one [a-z] character`);
   }
 
-  return result.join('');
+  function getValidIndex(): number {
+    const whiteListIndex = [...input.matchAll(/[a-z]{1}/gi)]
+      .map(({ index }) => index)
+      .filter((index) => index !== undefined && index !== null) as number[];
+    const index = randomNumber(0, whiteListIndex.length - 1);
+
+    return whiteListIndex[index];
+  }
+
+  function randomizeInput(): string {
+    const charactersList: string[] = input.split('');
+    for (let i = 0; i < times; i += 1) {
+      charactersList[getValidIndex()] = String.fromCharCode(randomNumber(97, 122));
+    }
+    return charactersList.join('');
+  }
+
+  return randomizeInput();
 }
