c096c242ac29e0a9ae39647548b7a40f90dc53c1
WarNote <andre_20113meira@hotmail.com>
2020-09-28 16:42:28 -0300

    Updated input arguments

--- src/utils/stringRandomizer.ts
+++ src/utils/stringRandomizer.ts
@@ -2,14 +2,24 @@ function randomNumber(lower = 0, upper = 1): number {
   return Math.round((Math.random() * (upper - lower)) + lower);
 }
 
+interface Options {
+  uppercase?: boolean,
+  times?: number,
+}
+
 /**
- * Rolls n times random letters in the string
+ * Rolls n times random letters in the string.
  *
- * @param { string } input string to randomize
- * @param { number } times how much times to random
+ * @param { string } input String to randomize.
+ *
+ * @param {{ uppercase: boolean, times: number }} options Options to define the output.
+ * @param { boolean } options.uppercase If true then the output may have uppercase letters.
+ * @param { number } options.times how much times to random
  *
  */
-export function stringRandomizer(input: string, times = 1): string {
+
+export function stringRandomizer(input: string, options?: Options): string {
+  const { uppercase = false, times = 1 } = options || {};
   if (!/[a-z]/gi.test(input)) {
     throw new Error(`Invalid input string: ${input} must have at least one [a-z] character`);
   }
@@ -26,7 +36,10 @@ export function stringRandomizer(input: string, times = 1): string {
   function randomizeInput(): string {
     const charactersList: string[] = input.split('');
     for (let i = 0; i < times; i += 1) {
-      charactersList[getValidIndex()] = String.fromCharCode(randomNumber(97, 122));
+      let charRange: [number, number] = [97, 122];
+      if (uppercase && randomNumber() === 1) charRange = [65, 90];
+
+      charactersList[getValidIndex()] = String.fromCharCode(randomNumber(...charRange));
     }
     return charactersList.join('');
   }
