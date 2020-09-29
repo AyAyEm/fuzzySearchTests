e0a0c5f8b53d8e66c7083804903931a845179da6
WarNote <andre_20113meira@hotmail.com>
2020-09-29 14:45:48 -0300

    Removed unecessary eslint comment

--- src/utils/inputs.ts
+++ src/utils/inputs.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/prefer-default-export
 export const inputs: string[] = [
   'ignis', 'valkyr', 'rhino', 'valkyr prime', 'aksomati', 'soma',
   'soma prime', 'akbolto', 'flow', 'carrier', 'carrier prime',
