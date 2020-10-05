95e97bdd0a4cbacb2cea25696ea9834c418cc4e9
WarNote <andre_20113meira@hotmail.com>
2020-10-04 21:31:16 -0300

    Added javascript Map to the tests

---
+++ tests/map.ts
@@ -0,0 +1,20 @@
+import Test from './_baseTest';
+
+class MapTest extends Test {
+  public testName = 'Map';
+
+  private nameIndexMap = new Map(this.items.map(
+    ({ namePrepared }, index) => [namePrepared, index],
+  ));
+
+  constructor(public times: number, public getInput: () => string) { super(); }
+
+  async testFunc(input: string) {
+    const itemIndex = this.nameIndexMap.get(input);
+    return itemIndex ? [this.items[itemIndex]] : [];
+  }
+}
+
+export default function mapTest(times: number, getInput: () => string) {
+  return new MapTest(times, getInput).exec();
+}
